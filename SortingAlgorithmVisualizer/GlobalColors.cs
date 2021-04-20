aa8bf57ce57079edb55d6abde0ae04a074357d21
tartie sorel <sorelaugustin@gmail.com>
2021-04-20 21:06:42 +0100

    button style implemented

---
+++ SortingAlgorithmVisualizer/GlobalColors.cs
@@ -0,0 +1,8 @@
+﻿namespace SortingAlgorithmVisualizer
+{
+    static class GlobalColors
+    {
+        public static string BackgroundColor { get; set; } = "Black";
+        public static string ForegroundColor { get; set; } = "#FFe67e22";
+    }
+}
