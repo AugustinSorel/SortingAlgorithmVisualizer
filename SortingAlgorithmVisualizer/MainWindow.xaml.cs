6a04e6b01eaddf746dc7aff8ef095c26ef5ed1f3
tartie sorel <sorelaugustin@gmail.com>
2021-04-20 19:54:11 +0100

    show array

--- SortingAlgorithmVisualizer/MainWindow.xaml.cs
+++ SortingAlgorithmVisualizer/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SortingAlgorithmVisualizer
@@ -20,15 +12,51 @@ namespace SortingAlgorithmVisualizer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int[] randomInts;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void SetUpArray()
+        {
+            randomInts = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < randomInts.Length; i++)
+                randomInts[i] = random.Next(0, (int)canvas.ActualHeight);
+        }
+
+        #region Key Down Event
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Escape)
                 Application.Current.Shutdown();
+        }
+        #endregion
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            SetUpArray();
+            DisplayTheArrayWithRectangles();
+        }
+
+        private void DisplayTheArrayWithRectangles()
+        {
+            for (int i = 0; i < randomInts.Length; i++)
+            {
+                Rectangle rectangle = new Rectangle()
+                {
+                    Height = randomInts[i],
+                    Width = canvas.ActualWidth / randomInts.Length,
+                    Fill = Brushes.White,
+                    StrokeThickness = 1,
+                    Stroke = Brushes.Green,
+                };
+
+                canvas.Children.Add(rectangle);
+                Canvas.SetLeft(rectangle, i * rectangle.Width);
+                Canvas.SetTop(rectangle, canvas.ActualHeight - rectangle.Height);
+            }
         }
     }
 }
