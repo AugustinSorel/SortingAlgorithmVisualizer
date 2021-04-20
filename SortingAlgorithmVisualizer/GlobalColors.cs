b011c9074fbd077b2127803c347f78e134a4486c
tartie sorel <sorelaugustin@gmail.com>
2021-04-20 21:10:30 +0100

    button style cleaner code

--- SortingAlgorithmVisualizer/GlobalColors.cs
+++ SortingAlgorithmVisualizer/GlobalColors.cs
@@ -2,7 +2,9 @@
 {
     static class GlobalColors
     {
+        private const string FOREGROUND_COLOR = "27ae60";
+
         public static string BackgroundColor { get; set; } = "Black";
-        public static string ForegroundColor { get; set; } = "#FFe67e22";
+        public static string ForegroundColor { get; set; } = "#FF" + FOREGROUND_COLOR;
     }
 }
