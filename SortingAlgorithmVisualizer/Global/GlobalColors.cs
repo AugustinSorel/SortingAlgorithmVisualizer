04dedba4ef66ae7b03daba0c6ad495014f350778
tartie sorel <sorelaugustin@gmail.com>
2021-04-20 21:39:25 +0100

    button start added

--- SortingAlgorithmVisualizer/Global/GlobalColors.cs
+++ SortingAlgorithmVisualizer/Global/GlobalColors.cs
@@ -2,7 +2,7 @@
 {
     static class GlobalColors
     {
-        private const string FOREGROUND_COLOR = "27ae60";
+        private const string FOREGROUND_COLOR = "1abc9c";
 
         public static string BackgroundColor { get; set; } = "Black";
         public static string ForegroundColor { get; set; } = "#FF" + FOREGROUND_COLOR;
