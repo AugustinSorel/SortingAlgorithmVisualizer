a69f3d6293fcd514632a2f87a5b0b5079204c8f6
tartie sorel <sorelaugustin@gmail.com>
2021-04-21 18:33:38 +0100

    slider implemented

--- SortingAlgorithmVisualizer/Global/GlobalColors.cs
+++ SortingAlgorithmVisualizer/Global/GlobalColors.cs
@@ -2,10 +2,10 @@
 {
     static class GlobalColors
     {
-        private const string FOREGROUND_COLOR = "1abc9c";
+        private const string FOREGROUND_COLOR = "FFFFFF";
 
         public static string BackgroundColor { get; set; } = "black";
-        public static string StripsColor { get; set; } = "white";
+        public static string StripsColor { get; set; } = "#FF" + FOREGROUND_COLOR;
         public static string ForegroundColor { get; set; } = "#FF" + FOREGROUND_COLOR;
     }
 }
