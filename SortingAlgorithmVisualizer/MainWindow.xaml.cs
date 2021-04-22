50266e794ada7067ca42dda432bdeffec777dfd2
tartie sorel <sorelaugustin@gmail.com>
2021-04-22 21:52:46 +0100

    cleaner code

--- SortingAlgorithmVisualizer/MainWindow.xaml.cs
+++ SortingAlgorithmVisualizer/MainWindow.xaml.cs
@@ -1,66 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace SortingAlgorithmVisualizer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private int[] randomInts;
-        private int arraySize;
-
-        public int ArraySize
-        {
-            get { return arraySize; }
-            set 
-            {
-                if (value != arraySize && value > 0 && value < 101)
-                {
-                    arraySize = value;
-                    NotifyPropertyChanged("ArraySize");
-                }
-            }
-        }
+        private MainWindowViewModel mainWindowViewModel;
 
         public MainWindow()
         {
             InitializeComponent();
-            PopulateComboBox();
-            DataContext = this;
-            arraySize = 10;
+            mainWindowViewModel = new MainWindowViewModel(algoNameComboBox);
+            DataContext = mainWindowViewModel;
         }
 
-        private void PopulateComboBox()
+        private void Window_ContentRendered(object sender, EventArgs e)
         {
-            List<string> ClassList = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(x => x.GetTypes())
-                .Where(x => typeof(ISortAlo).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
-                .Select(x => x.Name)
-                .ToList();
-            ClassList.Sort();
-
-            foreach (string entry in ClassList)
-                algoNameComboBox.Items.Add(entry);
-
-            algoNameComboBox.SelectedIndex = 0;
+            mainWindowViewModel.DisplayArray(canvas);
         }
 
-        private void SetUpArray()
+        private void ArraySizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            randomInts = new int[arraySize];
-            Random random = new Random();
-            for (int i = 0; i < randomInts.Length; i++)
-                randomInts[i] = random.Next(0, (int)canvas.ActualHeight);
+            if (mainWindowViewModel != null)    
+                mainWindowViewModel.DisplayArray(canvas);
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Pause");
+        }
+
+        private void AbortButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Abort");
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sortEngine = algoNameComboBox.SelectedItem.ToString();
+
+            Type type = Type.GetType(Assembly.GetEntryAssembly().GetName().Name + "." + sortEngine);
+
+            ConstructorInfo[] ctors = type.GetConstructors();
+
+            try
+            {
+                ISortAlo se = (ISortAlo)ctors[0].Invoke(null);
+                while (!se.IsSorted())//&& (!bgw.CancellationPending))
+                {
+                    se.NextStep();
+                }
+            }
+            catch
+            {
+                MessageBox.Show(":(");
+            }
+
         }
 
         #region Key Down Event
@@ -80,82 +80,5 @@ namespace SortingAlgorithmVisualizer
                 arraySizeSlider.Value += 1;
         }
         #endregion
-
-        private void Window_ContentRendered(object sender, EventArgs e)
-        {
-            DisplayArray();
-        }
-
-        private void DisplayArray()
-        {
-            SetUpArray();
-            AddRectanglesToCanvas();
-        }
-
-        private void AddRectanglesToCanvas()
-        {
-            canvas.Children.Clear();
-            for (int i = 0; i < randomInts.Length; i++)
-            {
-                Rectangle rectangle = new Rectangle()
-                {
-                    Height = randomInts[i],
-                    Width = canvas.ActualWidth / randomInts.Length,
-                    Fill = new BrushConverter().ConvertFromString(GlobalColors.BackgroundColor) as SolidColorBrush,
-                    StrokeThickness = 1,
-                    Stroke = new BrushConverter().ConvertFromString(GlobalColors.StripsColor) as SolidColorBrush,
-                };
-
-                canvas.Children.Add(rectangle);
-                Canvas.SetLeft(rectangle, i * rectangle.Width);
-                Canvas.SetTop(rectangle, canvas.ActualHeight - rectangle.Height);
-            }
-        }
-
-        private void PauseButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Pause");
-        }
-
-        private void AbortButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Abort");
-        }
-
-        private void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            string sortEngine = algoNameComboBox.SelectedItem.ToString();
-
-            Type type = Type.GetType(Assembly.GetEntryAssembly().GetName().Name + "." + sortEngine);
-            
-            ConstructorInfo[] ctors = type.GetConstructors();
-
-            try
-            {
-                ISortAlo se = (ISortAlo)ctors[0].Invoke(null);
-                while (!se.IsSorted() )//&& (!bgw.CancellationPending))
-                {
-                    se.NextStep();
-                }
-            }
-            catch
-            {
-                MessageBox.Show(":(");
-            }
-
-        }
-
-        #region Property Changed Event Handler
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void NotifyPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
-
-        private void ArraySizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            DisplayArray();
-        }
     }
 }
---
+++ SortingAlgorithmVisualizer/MainWindowViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SortingAlgorithmVisualizer
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private int[] randomInts;
+        private int arraySize;
+
+        public int ArraySize
+        {
+            get { return arraySize; }
+            set
+            {
+                if (value != arraySize && value > 0 && value < 101)
+                {
+                    arraySize = value;
+                    NotifyPropertyChanged("ArraySize");
+                }
+            }
+        }
+        public MainWindowViewModel(ComboBox comboBox)
+        {
+            arraySize = 10;
+            PopulateComboBox(comboBox);
+        }
+
+        private void PopulateComboBox(ComboBox comboBox)
+        {
+            List<string> ClassList = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => typeof(ISortAlo).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+                .Select(x => x.Name)
+                .ToList();
+            ClassList.Sort();
+
+            foreach (string entry in ClassList)
+                comboBox.Items.Add(entry);
+
+            comboBox.SelectedIndex = 0;
+        }
+
+        internal void DisplayArray(Canvas canvas)
+        {
+            SetUpArray(canvas.ActualHeight);
+            AddRectanglesToCanvas(canvas);
+        }
+
+        private void SetUpArray(double actualHeight)
+        {
+            randomInts = new int[arraySize];
+            Random random = new Random();
+            for (int i = 0; i < randomInts.Length; i++)
+                randomInts[i] = random.Next(0, (int)actualHeight);
+        }
+
+        private void AddRectanglesToCanvas(Canvas canvas)
+        {
+            canvas.Children.Clear();
+            for (int i = 0; i < randomInts.Length; i++)
+            {
+                Rectangle rectangle = new Rectangle()
+                {
+                    Height = randomInts[i],
+                    Width = canvas.ActualWidth / randomInts.Length,
+                    Fill = new BrushConverter().ConvertFromString(GlobalColors.BackgroundColor) as SolidColorBrush,
+                    StrokeThickness = 1,
+                    Stroke = new BrushConverter().ConvertFromString(GlobalColors.StripsColor) as SolidColorBrush,
+                };
+
+                canvas.Children.Add(rectangle);
+                Canvas.SetLeft(rectangle, i * rectangle.Width);
+                Canvas.SetTop(rectangle, canvas.ActualHeight - rectangle.Height);
+            }
+        }
+
+        #region Property Changed Event Handler
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
