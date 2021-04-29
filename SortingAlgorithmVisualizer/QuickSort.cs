0173d45748b4c982f9387a48378c5c0b674e7422
tartie sorel <sorelaugustin@gmail.com>
2021-04-29 21:53:47 +0100

    quick sort test

--- SortingAlgorithmVisualizer/QuickSort.cs
+++ SortingAlgorithmVisualizer/QuickSort.cs
@@ -19,9 +19,21 @@ namespace SortingAlgorithmVisualizer
 
         }
 
+        private void QuickSort1(int[] arr, int start, int end)
+        {
+            if (start >= end)
+                return;
+
+            var index = Partition(arr, start, end);
+            QuickSort1(arr, start, index - 1);
+            QuickSort1(arr, index + 1, end);
+        }
+
+
+
         public void NextStep()
         {
-
+            QuickSort1(randomInts, 0, randomInts.Length - 1);
         }
 
         public bool IsSorted()
